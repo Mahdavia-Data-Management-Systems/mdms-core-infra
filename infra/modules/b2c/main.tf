4ca46b749749abf0a3c82d85a46547e1dfebcc15
Noor Mahdi <noor.mahdi@datatorque.com>
2026-04-18 23:16:18 +1200

    fixed formatting

--- infra/modules/b2c/main.tf
+++ infra/modules/b2c/main.tf
@@ -5,11 +5,11 @@ resource "azurerm_resource_group" "b2c" {
 }
 
 resource "azurerm_aadb2c_directory" "this" {
-  resource_group_name    = azurerm_resource_group.b2c.name
-  domain_name            = var.domain_name
-  display_name           = var.display_name
-  sku_name               = var.sku_name
-  country_code           = var.country_code
+  resource_group_name     = azurerm_resource_group.b2c.name
+  domain_name             = var.domain_name
+  display_name            = var.display_name
+  sku_name                = var.sku_name
+  country_code            = var.country_code
   data_residency_location = var.data_residency_location
-  tags                   = var.tags
+  tags                    = var.tags
 }
