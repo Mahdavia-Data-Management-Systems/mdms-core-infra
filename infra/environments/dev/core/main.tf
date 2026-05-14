a7ca003cdb891c9e98d19562b03be5a6537bf690
Noor Mahdi <noormahdi@live.com>
2026-05-15 00:42:01 +1200

    fixed module path

--- infra/environments/dev/core/main.tf
+++ infra/environments/dev/core/main.tf
@@ -1,5 +1,5 @@
 module "entra" {
-  source = "../../modules/entra"
+  source = "../../../modules/entra"
 
   resource_group_name = "rg-mdms-dev-si-01"
   domain_name         = "mahdavisonlinedev.onmicrosoft.com"
--- infra/environments/prod/core/main.tf
+++ infra/environments/prod/core/main.tf
@@ -1,5 +1,5 @@
 module "entra" {
-  source = "../../modules/entra"
+  source = "../../../modules/entra"
 
   resource_group_name = "rg-mdms-prod-si-01"
   domain_name         = "mahdavisonline.onmicrosoft.com"
